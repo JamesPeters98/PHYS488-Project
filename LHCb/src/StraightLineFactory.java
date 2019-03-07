267c19be6c879cdb455e19c2ce4bd419d16e173a
James <jameslfc19@gmail.com>
2019-03-07 00:03:56 +0000

    Implemented Straight Line Factory
    Implemented Algorithm to fit a line to a set of 3D points from https://www.geometrictools.com/Documentation/LeastSquaresFitting.pdf

---
+++ LHCb/src/StraightLineFactory.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.BlockRealMatrix;
+import org.apache.commons.math3.linear.EigenDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+public class StraightLineFactory {
+	
+	RealVector mean;
+	int dimensionSize = 0;
+	EigenDecomposition ed;
+	
+	//Find straight from set of 3D points (Use ArrayRealVector).
+	public StraightLineFactory(RealVector[] vectors) throws Exception {
+		
+		//Take first vectors dimensions as the dimension size for the set.
+		if(dimensionSize == 0) dimensionSize = vectors[0].getDimension();
+		
+		//Calculate the mean of the group of vectors.
+		mean = new ArrayRealVector(dimensionSize);
+		for(RealVector v : vectors) {
+			if(v.getDimension() != dimensionSize) throw new Exception("Dimension of vector not :"+dimensionSize+" for vector "+v);
+			mean = mean.add(v);
+		}
+		mean.mapDivideToSelf(vectors.length);
+		
+		//Compute the covariance of the vectors as a Matrix
+		RealMatrix mat = null;
+		
+		for (int i = 0 ; i < vectors.length ; ++i )
+		{	
+			RealMatrix m = vectors[i].outerProduct(vectors[i]);
+			if(mat == null) mat = m;
+			mat = mat.add(m);
+		}
+		
+		ed = new EigenDecomposition(mat);
+		
+		System.out.println(Arrays.toString(ed.getRealEigenvalues()));
+	}
+	
+	public RealVector getDirectionVector() {
+		return ed.getEigenvector(0);
+	}
+	
+	public RealVector getOriginVector() {
+		return mean;
+	}
+	
+	public boolean isUnique() {
+		return ed.getRealEigenvalue(dimensionSize-1) < ed.getRealEigenvalue(dimensionSize-2);
+	}
+
+}
--- LHCb/src/Tests.java
+++ LHCb/src/Tests.java
@@ -1,20 +1,20 @@
-import flanagan.analysis.Regression;
+import org.apache.commons.math3.linear.ArrayRealVector;
 
 public class Tests {
-	
-	public static void main(String[] args) {
-		Regression reg = new Regression();
+
+	public static void main(String[] args) throws Exception {
+		ArrayRealVector vec[] = { 
+				new ArrayRealVector(new double[] {1,1,1}),
+				new ArrayRealVector(new double[] {2,2,2}),
+				new ArrayRealVector(new double[] {3,3,3})
+				};
 		
-		double x[][] = {{1,1},{2,2},{3,3}};
-		double y[][] = {{1,1},{2,2},{3,3}};
-		double z[][] = {{1,1},{2,2},{3,3}};
+		StraightLineFactory factory = new StraightLineFactory(vec);
 		
-		System.out.println(x.length);
-		System.out.println(y.length);
-		System.out.println(z.length);
-		
-		reg.enterData(x, y);
-		reg.linearGeneral();
+		System.out.println(factory.getDirectionVector());
+		System.out.println(factory.getOriginVector());
+		System.out.println("Unique: "+factory.isUnique());
+
 	}
 
 }
