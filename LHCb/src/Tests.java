8fded9a4afb691f6b8dc71d9f458b7b3b63fc280
jameslfc19 <jameslfc19@gmail.com>
2019-03-13 17:47:35 +0000

    Clean up

--- LHCb/src/Tests.java
+++ LHCb/src/Tests.java
@@ -1,15 +1,8 @@
-import org.apache.commons.math3.geometry.euclidean.threed.Line;
-import org.apache.commons.math3.geometry.euclidean.threed.Plane;
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-import org.apache.commons.math3.linear.ArrayRealVector;
-
 public class Tests {
 
 	public static void main(String[] args) throws Exception {
-				
-		SimulateSmearing smear = new SimulateSmearing(new ArrayRealVector(new double[] {0,0,0}), new ArrayRealVector(new double[] {1,2,1}), 0.5, 20);
-		StraightLineFactory factory1 = new StraightLineFactory(smear.getVectors());
-		
+		SimulateSmearing smear = new SimulateSmearing(Utils.getVector(0, 0, 0), Utils.getVector(1, 1, 1), 0.1, 19);
+		StraightLineFactory line = new StraightLineFactory(smear.getVectors());
 	}
 
 }
--- LHCb/src/Utils.java
+++ LHCb/src/Utils.java
@@ -1,10 +1,15 @@
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
 
 public class Utils {
 	
 	public static Vector3D toVector3D(RealVector v) {
 		return new Vector3D(v.toArray());
+	}
+	
+	public static RealVector getVector(double x, double y, double z) {
+		return new ArrayRealVector(new double[] {x,y,z});
 	}
 
 }
