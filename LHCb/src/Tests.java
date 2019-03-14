03ef145bec7415613abfafbdb5679f464013ccc0
James <jameslfc19@gmail.com>
2019-03-14 17:26:28 +0000

    Added Orson Chart Library.

--- LHCb/src/Tests.java
+++ LHCb/src/Tests.java
@@ -1,8 +1,42 @@
+import java.util.Random;
+
+import org.apache.commons.math3.linear.RealVector;
+
+import graphs.Graph;
+
 public class Tests {
+	
+	static RealVector[] a;
+	static RealVector[] d;
+	static RealVector[][] lines;
 
 	public static void main(String[] args) throws Exception {
-		SimulateSmearing smear = new SimulateSmearing(Utils.getVector(0, 0, 0), Utils.getVector(1, 1, 1), 0.1, 19);
-		StraightLineFactory line = new StraightLineFactory(smear.getVectors());
+		int n = 4;
+		a = new RealVector[n];
+		d = new RealVector[n];
+		lines = new RealVector[n][];
+		
+		Random rand = new Random();
+		double x = (rand.nextDouble()-.5);
+		double y = (rand.nextDouble()-.5);
+		double z = (rand.nextDouble()-.5);
+		
+		for(int i = 0; i < n; i++) {
+			StraightLineFactory line = createLine(x,y,z, (rand.nextDouble()-.5),rand.nextDouble()-.5,rand.nextDouble()-.5,0.01);
+			lines[i] = line.rawVectors;
+			a[i] = line.getOriginVector();
+			d[i] = line.getDirectionVector();
+		}
+		
+		FindNearestPoint p = new FindNearestPoint(a, d, 3);
+		
+		new Graph("Test",lines,a,d);
+	}
+	
+	public static StraightLineFactory createLine(double ax, double ay, double az,
+			double dx, double dy, double dz, double smear) throws Exception {
+		SimulateSmearing smearValues = new SimulateSmearing(Utils.getVector(ax,ay,az), Utils.getVector(dx,dy,dz), smear, 19);
+		return new StraightLineFactory(smearValues.getVectors());
 	}
 
 }
---
+++ LHCb/src/graphs/Graph.java
@@ -0,0 +1,137 @@
+package graphs;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.apache.commons.math3.linear.RealVector;
+
+import com.orsoncharts.Chart3DPanel;
+import com.orsoncharts.Colors;
+import com.orsoncharts.Chart3D;
+import com.orsoncharts.Chart3DFactory;
+import com.orsoncharts.data.xyz.XYZDataset;
+import com.orsoncharts.data.xyz.XYZSeries;
+import com.orsoncharts.data.xyz.XYZSeriesCollection;
+import com.orsoncharts.graphics3d.swing.DisplayPanel3D;
+import com.orsoncharts.plot.XYZPlot;
+import com.orsoncharts.renderer.xyz.ScatterXYZRenderer;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * A demo of a 3D line chart.
+ */
+@SuppressWarnings("serial")
+public class Graph extends JFrame {
+	
+	RealVector[][] lines;
+	RealVector[] a,d;
+
+    /**
+     * Creates a new test app.
+     *
+     * @param title  the frame title.
+     */
+    public Graph(String title, RealVector[][] lines, RealVector[] a, RealVector[] d) {
+        super(title);
+        this.lines = lines;
+        this.a = a;
+        this.d = d;
+        addWindowListener(new ExitOnClose());
+        getContentPane().add(createDemoPanel());
+        
+        pack();
+        setVisible(true);
+    }
+
+    private XYZDataset<String> createRawDataset() {
+        XYZSeriesCollection<String> dataset = new XYZSeriesCollection<String>();
+        for(int i = 0; i < lines.length; i++) {
+	        XYZSeries<String> series = new XYZSeries<String>("Series "+i);
+	        for(RealVector p : lines[i]) {
+	        	series.add(p.getEntry(0),p.getEntry(1),p.getEntry(2));
+	        }
+	        dataset.add(series);
+        }
+        
+        for(int i = 0; i < a.length; i++) {
+	        XYZSeries<String> series = new XYZSeries<String>("Line "+i);
+	        int steps = 3000;
+	        int range = 5;
+	        for(int j = -steps/2; j <= steps/2; j++) {
+	        	series.add(
+		        		a[i].getEntry(0)+d[i].getEntry(0)*range*(2*j)/steps,
+		        		a[i].getEntry(1)+d[i].getEntry(1)*range*(2*j)/steps,
+		        		a[i].getEntry(2)+d[i].getEntry(2)*range*(2*j)/steps);
+	        }
+	        
+	        dataset.add(series);
+        }
+        return dataset;
+    }
+    
+    
+    private Chart3D createScatterChart(XYZDataset<?> dataset) {
+        Chart3D chart = Chart3DFactory.createScatterChart("Raw Data", "", dataset, "X", "Y", "Z");
+        chart.setChartBoxColor(new Color(255, 255, 255, 128));
+        XYZPlot plot = (XYZPlot) chart.getPlot();
+        plot.getXAxis().setRange(-1, 1);
+        plot.getYAxis().setRange(-1, 1);
+        plot.getZAxis().setRange(-1, 1);
+        
+        ScatterXYZRenderer renderer = (ScatterXYZRenderer) plot.getRenderer();
+        renderer.setSize(0.02);
+        renderer.setColors(Colors.createIntenseColors());
+        return chart;    
+    }
+    
+  
+    
+    /**
+     * Returns a panel containing the content for the demo.  This method is
+     * used across all the individual demo applications to allow aggregation 
+     * into a single "umbrella" demo (OrsonChartsDemo).
+     * 
+     * @return A panel containing the content for the demo.
+     */
+    public JPanel createDemoPanel() {
+        Panel content = new Panel(new BorderLayout());
+        Dimension size = new Dimension(1280, 720);
+        content.setPreferredSize(size);
+        
+        //Raw Data
+        XYZDataset dataset = createRawDataset();
+        Chart3D chart = createScatterChart(dataset);
+        Chart3DPanel chartPanel = new Chart3DPanel(chart);
+        content.setChartPanel(chartPanel);
+        chartPanel.zoomToFit(size);
+        content.add(new DisplayPanel3D(chartPanel));
+        return content;
+    }
+
+//    /**
+//     * Starting point for the app.
+//     *
+//     * @param args  command line arguments (ignored).
+//     */
+//    public static void main(String[] args) {
+//    	Graph app = new Graph(
+//                "OrsonCharts: XYZLineChart3DDemo2.java");
+//        app.pack();
+//        app.setVisible(true);
+//    }
+    
+    public class ExitOnClose extends WindowAdapter {
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+        
+    }
+}
+
